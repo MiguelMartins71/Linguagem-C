78574c317efd5c1fad23eaa734bea4053e5d26f5
Miguel Henrique Martins Carvalho <121458728+MiguelMartins71@users.noreply.github.com>
2022-12-26 17:01:40 -0300

    Add files via upload

---
+++ programa.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+  int main(){
+    // imprime o cabeçalho do jogo
+  	printf ("*******************************\n");
+    printf("*Bem vindo ao jogo de advinhação*\n");
+     printf("********************************\n");
+     int numerosecreto = 42;
+     int chute;
+     printf("Qual é o seu chute?");
+     scanf("%d" , &chute);
+     printf("Seu chute foi %d\n",chute);
+
+}
+
+
+
+
+
