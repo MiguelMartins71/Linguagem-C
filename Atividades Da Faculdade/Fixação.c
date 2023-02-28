30a80a050ae56aae9872745bfef080446e7f7424
Miguel Martins <121458728+MiguelMartins71@users.noreply.github.com>
2023-02-28 09:07:13 -0300

    Add files via upload

---
+++ Atividades Da Faculdade/Fixação.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main(){
+    
+    // Exercício 1
+
+    // Fazer um programa para ler dois valores e mostrar a sua média
+    
+    /*
+    float a,b;
+
+    printf("Digite o Primeiro Número");
+
+    scanf("%f", &a);
+    printf("Digite o Segundo Número:");
+    scanf("%f", &b);
+    float media = a + b/2;
+
+    printf("A Média doa Valores será %f", media);
+    */
+
+    // Exercício 2 
+    // Escreva um algoritmo que solicite ao usuário a altura e o raio de um cilindro circular e imprima o volume do cilindro
+    /*
+    float altura,raio,vf;
+    float PI = 3.14;
+    printf("Digite a altura");
+    scanf("%f", &altura);
+    printf("Digite o Raio");
+    scanf("%f", &raio); 
+    vf = PI + raio * 2 + altura;
+    printf("O Volume do Cilindro Circular será : %f\n", vf);
+    */
+   
+    // Exercício 3
+   // Uma empresa contrata um encanador a R$ 20.00 por dia. Crie um programa que solicite o número de dias trabalhados 
+  //  pelo encanador e imprima a quantia líquida que deverá ser paga, sabendo-se que são descontados 8% de impostos 
+  /*
+  int valor = 20.00;
+  int dias;
+  int calc;
+  printf("Quantos Dias Foram Trabalhados?");
+  scanf("%d", &dias);
+  calc = valor * dias;
+   printf("O Salário Será : %d\n", calc);
+  */
+
+   // Exercício 4 
+  // O cardápio de uma lanchonete é dado abaixo. Prepare um programa que leia a quantidade de cada item que você 
+ // consumiu e calcule a conta final.
+   
+   /*int cod, qtd;
+
+     float total;
+
+     printf("Digite o codigo do pedido: \n");
+
+     scanf("%d", &cod);
+
+     printf("Digite a quantidade: \n");
+
+     scanf("%d",&qtd);
+
+     if( cod == 100) {
+
+       total = qtd * 3.0;
+
+        printf("O total eh : %f\n", total);
+
+     } else if( cod == 101) {
+
+       total = qtd * 3.5;
+
+        printf("O total eh : %f\n", total);
+
+     } else if( cod ==102) {
+
+       total = qtd * 4.0;
+
+        printf("O total eh : %f\n", total);
+
+     } else if( cod == 103) {
+
+       total = qtd * 4.2;
+
+        printf("O total eh : %f\n", total);
+
+     } else if( cod == 104) {
+
+       total = qtd * 4.5;
+
+        printf("O total eh : %f\n", total);
+
+     } else {
+
+       printf("O cod nao existe!");
+
+     }   */
+
+}
