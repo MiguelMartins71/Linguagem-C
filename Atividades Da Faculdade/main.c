994d2473f189350be7bac601758bb6f1dfe12e20
Miguel Martins <121458728+MiguelMartins71@users.noreply.github.com>
2023-02-27 21:37:06 -0300

    Add files via upload

---
+++ Atividades Da Faculdade/main.c
@@ -0,0 +1,112 @@
+//
+// Created by Miguel on 25/02/2023.
+//
+//---------------
+//Miguel Henrique Martins Carvalho
+//766634
+//---------------
+
+#include <stdio.h>
+#include <time.h>
+int main(){
+    //Exercício 1
+    /* int x,y,soma;
+     printf("Digite o Primeiro numero :");
+     scanf("%d", &x);
+     printf("Digite o Segundo Numero :");
+     scanf("%d", &y);
+     soma = x + y;
+     printf("A Soma dos Valores de %d e %d sera %d", x,y,soma);
+     */
+    //Exercício 2
+    /* float salario;
+    float salariominimo = 1380.60;
+    float qtndsalario;
+    printf("Digite seu salário");
+    scanf("%f", &salario);
+    qtndsalario = salario/salariominimo;
+    printf("O seu salário é igual a %.2f do Salário minimo declarado", qtndsalario);
+    return 0;
+     */
+    //Exercício 3
+    /*int CDU,c1,d1,d2,u1,c2,r1,UDC;
+
+    printf("Escreva os 3 Digitos:");
+    scanf("%d", &CDU);
+    c1 = CDU/100;
+    r1=CDU%100;
+    d1=r1/10;
+    u1=CDU%10;
+    c2=u1*100;
+    d2=d1*10;
+
+    UDC = c2 + d2 + c1;
+    printf("%d", UDC);
+
+     */
+
+    //Exercício 4
+    /*
+    int ano=1970;
+    int mes = 1;
+    int dia =1;
+
+    int anod,mesd,diad;
+
+    printf("Digite o ano :");
+    scanf("%d", &anod);
+
+    printf("Digite o mes :");
+    scanf("%d", &mes);
+
+    printf("Digite os dias :");
+    scanf("%d", &diad);
+
+
+   int calculoano,calculoano2,calculodia,calculominutos,calculosegundos;
+
+   diad = (anod-1970) * 365 + (mesd-1)*30 + (diad-1); 
+
+    calculosegundos= 60 * 60 * 24 - diad;
+
+
+   printf("Os segundos de diferença serão de %d: ", calculosegundos); */
+
+    
+    //Exercício 5
+
+    /* int CDU,c1,d1,d2,u1,c2,r1,UDC;
+
+    printf("Escreva os 3 Digitos:");
+    scanf("%d", &CDU);
+    c1 = CDU/100;
+    r1=CDU%100;
+    d1=r1/10;
+    u1=CDU%10;
+    c2=u1*100;
+    d2=d1*10;
+
+    UDC = c2 + d2 +c1;
+    printf("%d", UDC);
+
+    int calculo;
+
+    calculo = CDU + UDC;
+    printf("A soma dos dois será de %d\n", calculo);
+
+    int calc1,calc2,calc3;
+
+    calc1 = c1 * u1;
+
+    printf("a soma seria %d\n", calc1);
+
+    calc2 = d2 * d1/10;
+
+    printf("A soma so segundo digito será de %d\n", calc2);
+
+    calc3 = u1 * c1;
+
+    printf("A Soma do Terceiro digito sera de %d\n", calc3);
+     */
+
+}
